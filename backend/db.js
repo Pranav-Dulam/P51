2fd4734e40461188df6267bfad8c44fe2869ccf5
cowboy-bebop7 <113116323+cowboy-bebop7@users.noreply.github.com>
2025-12-06 20:55:47 -0500

    Fix DB IPv4 host

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ const mysql = require("mysql2");
 
 // Use same config for Mac + DigitalOcean
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "127.0.0.1", // force IPv4 to avoid ::1 MySQL issues
+  port: 3306, // ensure MySQL IPv4 port
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
